3f06bc66e1a4c4abb86c83ae151a825e4110c877
Astar <bahouidi99@gmail.com>
2024-07-14 17:14:19 +0100

    Navigating Between Pages
    - How to use the next/link component.
    - How to show an active link with the usePathname() hook.
    - How navigation works in Next.js.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,6 @@
 import { Revenue } from "./definitions";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export const formatCurrency = (amount: number) => {
   return (amount / 100).toLocaleString("en-US", {
@@ -9,7 +11,7 @@ export const formatCurrency = (amount: number) => {
 
 export const formatDateToLocal = (
   dateStr: string,
-  locale: string = "en-US",
+  locale: string = "en-US"
 ) => {
   const date = new Date(dateStr);
   const options: Intl.DateTimeFormatOptions = {
@@ -67,3 +69,7 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
     totalPages,
   ];
 };
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
