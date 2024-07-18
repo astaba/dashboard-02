4992230df747d3af5f8509f7c0a8f380eb4caafe
Astar <bahouidi99@gmail.com>
2024-07-18 12:27:16 +0100

    Adding search
    - Learn how to use the Next.js APIs: useSearchParams, usePathname, and useRouter.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <div>Invoices Page</div>;
+import { Suspense } from "react";
+
+import Pagination from "@/components/invoices/pagination";
+import Search from "@/components/search";
+import Table from "@/components/invoices/table";
+import { CreateInvoice } from "@/components/invoices/buttons";
+import { lusitana } from "@/lib/fonts";
+import { InvoicesTableSkeleton } from "@/components/skeletons";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    page?: string;
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+    </div>
+  );
 }
--- components/search.tsx
+++ components/search.tsx
@@ -1,8 +1,29 @@
 "use client";
-
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useDebouncedCallback } from "use-debounce";
+
+// NOTE: (1) Capture the user input with an inline handler
+// (2) Append the search term to the already existing URLSearchParams
+// if any, and push to the browser History API.
+// (3) Keep the URL in sync with input field via its defaultValue
+// (5) Debounce the event handler to avoid colapsing the database API
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const term = value.trim();
+    // TEST:
+    // console.log("Search Input:", term);
+    const params = new URLSearchParams(searchParams);
+    if (term) params.set("query", term);
+    else params.delete("query");
+    router.replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +32,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(event) => handleSearch(event.target.value)}
+        defaultValue={searchParams.get("query") as string}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
